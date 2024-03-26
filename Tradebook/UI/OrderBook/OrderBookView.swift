0500cad2f83b1721d03347cc989b49ccb7c1e7c7
Chaitanya Pandit <chaitanyapandit@me.com>
2024-03-26 21:06:34 +0530

    WIP

--- Tradebook/UI/OrderBook/OrderBookView.swift
+++ Tradebook/UI/OrderBook/OrderBookView.swift
@@ -8,19 +8,33 @@
 import SwiftUI
 
 struct OrderBookView: View {
-    @ObservedObject var viewModel = OrderBookViewModel()
+    @StateObject var viewModel = OrderBookViewModel()
 
     public var body: some View {
-        VStack() {
-            Text("Something")
-            ForEach($viewModel.buyOrders.orders) { order in
-                HStack {
-                    Text("\(order.size)")
-                    Text("\(order.price)")
+        HStack {
+            VStack() {
+                ForEach(viewModel.buyOrders) { order in
+                    HStack {
+                        Text("\(order.size)")
+                        Spacer()
+                        Text("\(order.price)")
+                    }
                 }
+
+                Spacer()
             }
 
-            Spacer()
+            VStack() {
+                ForEach(viewModel.sellOrders) { order in
+                    HStack {
+                        Text("\(order.size)")
+                        Spacer()
+                        Text("\(order.price)")
+                    }
+                }
+
+                Spacer()
+            }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(.yellow)
--- Tradebook/UI/OrderBook/OrderBookViewModel.swift
+++ Tradebook/UI/OrderBook/OrderBookViewModel.swift
@@ -7,43 +7,12 @@
 
 import Combine
 
-public class OrderBookHistory: ObservableObject {
-    enum SortOrder {
-        case ascending
-        case descending
-    }
-    
-    @Published var orders = [OrderBook.Item]()
-    let sortOrder: SortOrder
-    
-    init(sortOrder: SortOrder = .ascending) {
-        self.sortOrder = sortOrder
-    }
-    
-    func addItem(_ item: OrderBook.Item) {
-        var existingOrders = self.orders
-        existingOrders.append(item)
-
-        existingOrders.sort(by: {
-            switch sortOrder {
-            case .ascending:
-                $1.price > $0.price
-            case .descending:
-                $0.price > $1.price
-            }
-        })
-        
-        existingOrders = Array(existingOrders.prefix(20))
-        self.orders = existingOrders
-    }
-}
-
 public class OrderBookViewModel: ObservableObject {
     private var api: API<OrderBook>? = nil
     private var cancellables = Set<AnyCancellable>()
     
-    @Published var buyOrders = OrderBookHistory(sortOrder: .descending)
-    @Published var sellOrders = OrderBookHistory()
+    @Published var buyOrders = [OrderBook.Item]()
+    @Published var sellOrders = [OrderBook.Item]()
 
     init() {
         self.api = API()
@@ -60,10 +29,16 @@ public class OrderBookViewModel: ObservableObject {
         for item in order.data {
             switch item.side {
             case .buy:
-                buyOrders.addItem(item)
+                buyOrders.append(item)
             case .sell:
-                sellOrders.addItem(item)
+                sellOrders.append(item)
             }
         }
+        
+        buyOrders.sort(by: { $1.price > $0.price})
+        buyOrders = Array(buyOrders.prefix(20))
+
+        sellOrders.sort(by: { $0.price > $1.price})
+        sellOrders = Array(sellOrders.prefix(20))
     }
 }
